228ab37d0485ef1fca3cfa5528117ee781db2d26
prisciladuarte <duartecostap@gmail.com>
2021-12-17 14:47:38 -0300

    Cria Service

--- src/jogadores/jogadores.module.ts
+++ src/jogadores/jogadores.module.ts
@@ -1,7 +1,10 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { JogadoresController } from './jogadores.controller';
+import { JogadoresService } from './jogadores.service';
 
 @Module({
-  controllers: [JogadoresController]
+  controllers: [JogadoresController],
+  providers: [JogadoresService]
 })
 export class JogadoresModule {}
