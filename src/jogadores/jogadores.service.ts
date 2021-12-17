08b2411843344ddaeba2e5d38995567fd8363927
prisciladuarte <duartecostap@gmail.com>
2021-12-17 16:16:30 -0300

    cria metodos

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Injectable, Logger } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { Jogador } from './interfaces/jogador.interface';
+import * as uuid from 'uuid/v1'
+import { windowWhen } from 'rxjs';
 
 @Injectable()
-export class JogadoresService {}
+export class JogadoresService {
+
+    private jogadores: Jogador[] = [];
+
+    private readonly logger = new Logger(JogadoresService.name)
+    async criarAtualizarJogador(criaJogadorDto: CriarJogadorDto): Promise<void>{
+        this.logger.log(`criaJogadorDto: ${ criaJogadorDto }`)
+        await this.criar(criaJogadorDto)
+    }
+
+    private criar(criaJogadorDto: CriarJogadorDto): void {
+        const { nome, telefoneCelular, email } = criaJogadorDto
+
+        const jogador: Jogador = {
+            _id: uuid(),
+            nome,
+            telefoneCelular,
+            email,
+            ranking:'A',
+            posicaoRanking: 1,
+            urlFotoJogador: 'www.google.com.br/foto123.jpg'
+        }
+        this.jogadores.push(jogador)
+    }
+
+}
