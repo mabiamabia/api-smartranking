ff295a455eed6eaf39bb9b9343826bb9d70675d5
prisciladuarte <duartecostap@gmail.com>
2022-01-06 15:10:30 -0300

    Cria atualização

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -3,7 +3,6 @@ import { BadRequestException, Injectable, Logger, NotFoundException } from '@nes
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { AtualizarJogadorDto } from './dtos/atualizar-jogador.dto';
 import { Jogador } from './interfaces/jogador.interface';
-import {v4 as uuidv4 } from 'uuid' ;
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 @Injectable()
