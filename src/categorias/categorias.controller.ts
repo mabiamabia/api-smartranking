54b5f7522f44080b0e7e913632f1a3d6572eca9c
prisciladuarte <duartecostap@gmail.com>
2022-01-07 10:32:11 -0300

    retira Put

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Get, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
 import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 import { Categoria } from './interfaces/categoria.interface';
