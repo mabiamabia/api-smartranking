83e12ce73df7988bb2d1a363ebd26e30aa140590
prisciladuarte <duartecostap@gmail.com>
2022-02-12 21:10:12 -0300

    Finaliza

---
+++ src/desafios/desafios.controller.ts
@@ -0,0 +1,5 @@
+/* eslint-disable prettier/prettier */
+import { Desafio } from "./interfaces/desafio.interface";
+import { DesafioStatusValidacaoPipe } from './pipes/desafio-status-validation.pipe';
+import { AtribuirDesafioPartidaDto } from '.dtos/atribuir-desafio-partida.dto';
+
